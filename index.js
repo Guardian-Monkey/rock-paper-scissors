36d47e4a3c9c7b3e41bee26f7d74e4f70742d4a3
Paul Gutierrez <paulitoguti747@gmail.com>
2024-06-27 12:18:21 -0400

    Create the playGame function
    Move the global variables into the playGame function to change their
    scope; this makes it so that global scope is avoided.
    Move the playRound function into playGame. Change said function into an
    arrow function; this is done to avoid hoisting.

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// Variables track the score of the user, and the computer
-let humanScore = 0;
-let computerScore = 0;
-
 // Function returns the following string at random: rock, paper, or scissors
 function getComputerChoice() {
     // Multiply the random number by 3, and floor it so the max is whole; add 1 to make the range from 1 to 3
@@ -20,35 +16,41 @@ function getHumanChoice() {
     return input;
 }
 
-// Create a function that plays a round of rock paper scissors
-// The function prints the winner to the console, and increments the score of the winner by 1
-function playRound() {
-    let computerChoice = getComputerChoice();
-    let humanChoice = getHumanChoice();
-    if (computerChoice == humanChoice)
-        console.log("Draw!");
-    else if (humanChoice == 'rock' && computerChoice == 'paper') {
-        console.log("Computer Wins!");
-        computerScore++;
-    }
-    else if (humanChoice == 'rock' && computerChoice == 'scissors') {
-        console.log("You Win!");
-        humanScore++;
-    }
-    else if (humanChoice == 'paper' && computerChoice == 'rock') {
-        console.log("You Win!");
-        humanScore++;
-    }
-    else if (humanChoice == 'paper' && computerChoice == 'scissors') {
-        console.log("Computer Wins!");
-        computerScore++;
-    }
-    else if (humanChoice == 'scissors' && computerChoice == 'rock') {
-        console.log("Computer Wins!");
-        computerScore++;
-    }
-    else if (humanChoice == 'scissors' && computerChoice == 'paper') {
-        console.log("You Win!");
-        humanScore++;
+function playGame() {
+    // Variables track the score of the user, and the computer
+    let humanScore = 0;
+    let computerScore = 0;
+
+    // Create a function that plays a round of rock paper scissors
+    // The function prints the winner to the console, and increments the score of the winner by 1
+    const playRound = () => {
+        let computerChoice = getComputerChoice();
+        let humanChoice = getHumanChoice();
+        if (computerChoice == humanChoice)
+            console.log("Draw!");
+        else if (humanChoice == 'rock' && computerChoice == 'paper') {
+            console.log("Computer Wins!");
+            computerScore++;
+        }
+        else if (humanChoice == 'rock' && computerChoice == 'scissors') {
+            console.log("You Win!");
+            humanScore++;
+        }
+        else if (humanChoice == 'paper' && computerChoice == 'rock') {
+            console.log("You Win!");
+            humanScore++;
+        }
+        else if (humanChoice == 'paper' && computerChoice == 'scissors') {
+            console.log("Computer Wins!");
+            computerScore++;
+        }
+        else if (humanChoice == 'scissors' && computerChoice == 'rock') {
+            console.log("Computer Wins!");
+            computerScore++;
+        }
+        else if (humanChoice == 'scissors' && computerChoice == 'paper') {
+            console.log("You Win!");
+            humanScore++;
+        }
     }
 }
